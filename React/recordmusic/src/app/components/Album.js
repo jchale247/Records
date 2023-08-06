7e4a4f0d9424828e6c635ce492f3ad95972cce44
Joseph <joseph@Josephs-MacBook-Pro.local>
2023-08-05 22:17:40 -0500

    created Album component and working on formatting

--- React/recordmusic/src/app/components/Album.js
+++ React/recordmusic/src/app/components/Album.js
@@ -2,16 +2,16 @@ import Image from 'next/image'
 function Album({Img, Name, Artist}){
     return(
         
-        <div className="relative justify-center content-center flex rounded-lg h-72 w-52 bg-zinc-800
-                            hover:rounded-3xl transition-all duration-500 shadow-md">
+        <div className="group relative justify-center content-center flex rounded-lg h-72 w-52 bg-zinc-800
+                            hover:rounded-2xl transition-all duration-500 shadow-md">
             <Image
             src={Img}
             width={500}
             height={500}
             alt="Album Cover"
-            className='absolute top-4 h-48 w-48 rounded-lg'/>
-                <h1 className="absolute bottom-9 left-3 text-lg text-sky-100">{Name}</h1>
-                <h1 className="absolute bottom-5 left-3 text-xs text-sky-100">{Artist}</h1>
+            className='absolute top-4 h-48 w-48 rounded-lg group-hover:rounded-xl transition-all duration-500 '/>
+                <h1 className="absolute overflow-ellipsis whitespace-nowrap overflow-hidden bottom-9 left-3 text-lg text-sky-100">{Name}</h1>
+                <h1 className="absolute overflow-ellipsis overflow-hidden bottom-5 left-3 text-xs text-sky-100">{Artist}</h1>
             </div>
     );
 }
--- React/recordmusic/src/app/components/Feed.js
+++ React/recordmusic/src/app/components/Feed.js
@@ -3,10 +3,7 @@ import Album from './Album'
 function Feed(){
     return(
         <div className="flex space-x-4 justify-center items-center bg-zinc-900 h-screen">
-            <Album Img="https://i.scdn.co/image/ab67616d0000b2731bd6d088d3d81972af4cb81d" Name="Apricot Princess" Artist="Rex Orange County" />
-            <Album Img="https://i.scdn.co/image/ab67616d0000b2731bd6d088d3d81972af4cb81d" Name="Apricot Princess" Artist="Rex Orange County" />
-            <Album Img="https://i.scdn.co/image/ab67616d0000b2731bd6d088d3d81972af4cb81d" Name="Apricot Princess" Artist="Rex Orange County" />
-            <Album Img="https://i.scdn.co/image/ab67616d0000b2731bd6d088d3d81972af4cb81d" Name="Apricot Princess" Artist="Rex Orange County" />
+            <Album Img="https://i.scdn.co/image/ab67616d0000b273d970cd18ef2f579691cf56bc" Name="Live at Radio City Music Hall" Artist="Rex Orange County" />
             <Album Img="https://i.scdn.co/image/ab67616d0000b2731bd6d088d3d81972af4cb81d" Name="Apricot Princess" Artist="Rex Orange County" />
             <Album Img="https://i.scdn.co/image/ab67616d0000b2731bd6d088d3d81972af4cb81d" Name="Apricot Princess" Artist="Rex Orange County" />
         </div>
